fe0204780e67d518e93b64a657d9282286ddbc79
Jer-Sch <schuurm@ns.codes>
2021-08-17 15:50:50 -0500

    add assertions

--- app/grants/tests/models/test_cart_activity.py
+++ app/grants/tests/models/test_cart_activity.py
@@ -27,18 +27,23 @@ class TestCartActivity:
         cart_activity = CartActivityFactory()
 
         assert hasattr(cart_activity, "action")
+        assert cart_activity.action == ''
 
     def test_cart_activity_has_metadata_attribute(self):
         cart_activity = CartActivityFactory()
 
         assert hasattr(cart_activity, "metadata")
+        assert cart_activity.metadata == {}
+        assert len(cart_activity.metadata) == 0
     
     def test_cart_activity_has_bulk_attribute(self):
         cart_activity = CartActivityFactory()
 
         assert hasattr(cart_activity, "bulk")
+        assert cart_activity.bulk == False
 
     def test_cart_activity_has_latest_attribute(self):
         cart_activity = CartActivityFactory()
 
         assert hasattr(cart_activity, "latest")
+        assert cart_activity.latest == False
