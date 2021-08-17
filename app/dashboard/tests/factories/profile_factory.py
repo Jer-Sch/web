8c9c9baac9cca4fd053637ca26048f79aea84bcc
Jer-Sch <schuurm@ns.codes>
2021-08-17 14:21:05 -0500

    add factory for Profile

---
+++ app/dashboard/tests/factories/profile_factory.py
@@ -0,0 +1,11 @@
+import factory
+
+from dashboard.models import Profile
+
+
+class ProfileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Profile
+    
+    handle = factory.Sequence(lambda n: "Contributor_%03d" % n)
+    data = {}
