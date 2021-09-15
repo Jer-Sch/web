3137aa42eef72f5f280026a7f2149070682e5fc0
Jer-Sch <schuurm@ns.codes>
2021-09-15 14:07:10 -0500

    refactor

--- app/grants/tests/models/test_cart_activity.py
+++ app/grants/tests/models/test_cart_activity.py
@@ -11,38 +11,37 @@ class TestCartActivity:
     """Test CartActivity model."""
 
     def test_creation(self):
-        """
-        Test instance returned by CartActivityFactory is an instance of CartActivity
-        """
+        """Test instance returned by CartActivityFactory is valid."""
 
         cart_activity = CartActivityFactory()
 
         assert isinstance(cart_activity, CartActivity)
 
-    def test_cart_activity_belongs_to_grant(self):
-        """Test grant attribute in CartActivity model (ForeignKey)."""
+    def test_cart_activity_has_associated_grant(self):
+        """Test grant is present and is an instance of Grant."""
 
         cart_activity = CartActivityFactory()
 
+        assert hasattr(cart_activity, 'grant')
         assert isinstance(cart_activity.grant, Grant)
 
     def test_cart_activity_belongs_to_profile(self):
-        """Test profile attribute in CartActivity model (ForeignKey)."""
+        """Test profile is present and is an instance of Profile."""
 
         cart_activity = CartActivityFactory()
         
+        assert hasattr(cart_activity, 'profile')
         assert isinstance(cart_activity.profile, Profile)
 
     def test_cart_activity_has_action_attribute(self):
-        """Test action attribute and default value (CharField)"""
+        """Test action attribute is present."""
 
         cart_activity = CartActivityFactory()
 
         assert hasattr(cart_activity, "action")
-        assert cart_activity.action == ''
 
     def test_cart_activity_has_metadata_attribute(self):
-        """Test metadata attribute and default value (JSONField)"""
+        """Test metadata attribute is present and defaults to empty dictionary."""
 
         cart_activity = CartActivityFactory()
 
@@ -51,7 +50,7 @@ class TestCartActivity:
         assert len(cart_activity.metadata) == 0
     
     def test_cart_activity_has_bulk_attribute(self):
-        """Test bulk attribute and default value (BooleanField)"""
+        """Test bulk attribute is present and defaults to False."""
 
         cart_activity = CartActivityFactory()
 
@@ -59,7 +58,7 @@ class TestCartActivity:
         assert cart_activity.bulk == False
 
     def test_cart_activity_has_latest_attribute(self):
-        """Test latest attribute and default value (BooleanField)"""
+        """Test latest attribute is present and defaults to False."""
 
         cart_activity = CartActivityFactory()
 
